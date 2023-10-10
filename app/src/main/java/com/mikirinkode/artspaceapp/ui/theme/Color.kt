11841e91ad9c4bbc5f52b8301ef266d3b169444a
Muhammad Wafa <muhammadwafa105@gmail.com>
2023-10-10 16:03:05 +0700

    initial commit

---
+++ app/src/main/java/com/mikirinkode/artspaceapp/ui/theme/Color.kt
@@ -0,0 +1,12 @@
+package com.mikirinkode.artspaceapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+
+val Blue80 = Color(0xFF43B0FF)
+val Blue40 = Color(0xFF2083F8)
+
+val LightBackground = Color(0xFFE4EBFF)
+val LightPrimaryContainer = Color.White
+val DarkBackground = Color(0xFF03202C)
+val DarkPrimaryContainer = Color(0xFF29486a)
